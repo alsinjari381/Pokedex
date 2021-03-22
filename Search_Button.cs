0b543d9bdc842f5e6a085b3939eaf203b2de3aa1
alsinjari381 <76085879+alsinjari381@users.noreply.github.com>
2021-03-21 21:44:04 -0700

    Added OCR

--- Search_Button.cs
+++ Search_Button.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Windows.Forms;
 using System.Drawing;
 using System.Data.SQLite;
@@ -9,19 +8,35 @@ namespace Pokedex_GUI
 	public class Search_Button
 	{
 		public Button button { get; set; }
-
-		public Search_Button()
+		
+		public Search_Button(int x, int y, string text)
 		{
 			this.button = new Button();
-			this.button.Size = new Size(40, 40);
-			this.button.Location = new Point(30, 55);
-			this.button.Text = "Run";
+			this.button.Size = new Size(80, 40);
+			this.button.Location = new Point(x, y);
+			this.button.Text = text;
 		}
 
+
 		public void Execute_Text_Search(Creature_Input data, SQLiteConnection con)
+		{
+			string weakness_query = $"SELECT weaknesses FROM pokedex WHERE pokemon = \"{data.textBox.Text.ToLower().Trim()}\"";
+			string resistance_query = $"SELECT resistances FROM pokedex WHERE pokemon = \"{data.textBox.Text.ToLower().Trim()}\"";
+			Continue_Search(weakness_query, resistance_query, con);
+		}
+
+		public void Execute_OCR_Search(Creature_Input data, SQLiteConnection con)
+		{
+			string pokemon = Poke_Img.Analyze_Screenshot(data);
+			string weakness_query = $"SELECT weaknesses FROM pokedex WHERE pokemon = \"{pokemon.ToLower().Trim()}\"";
+			//MessageBox.Show(weakness_query);
+			string resistance_query = $"SELECT resistances FROM pokedex WHERE pokemon = \"{pokemon.ToLower().Trim()}\"";
+			Continue_Search(weakness_query, resistance_query, con);
+		}
+
+
+		private void Continue_Search(string weakness_query, string resistance_query, SQLiteConnection con)
         {
-			string weakness_query = $"SELECT weaknesses FROM pokedex WHERE pokemon = \"{data.textBox.Text.ToLower()}\"";
-			string resistance_query = $"SELECT resistances FROM pokedex WHERE pokemon = \"{data.textBox.Text.ToLower()}\"";
 			string weaknesses = "";
 			string resistances = "";
 
@@ -51,11 +66,11 @@ namespace Pokedex_GUI
                 }
 				con.Close();
             }
-
+			Poke_Img.Clear_Images();
+			
 			// if look-up returned any existing pokemon
 			if (!(weaknesses == "" && resistances == ""))
 			{ 
-				Poke_Img.Clear_Images();
 				Poke_Img.Upload_Images(weaknesses, "weaknesses");
 				Poke_Img.Upload_Images(resistances, "resistances");
 			}
